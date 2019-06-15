54b92844696b8df3e83c43690bda18600416a8b6
Sonthaya Boonchan <sonthaya.jackrobotic@mail.kmutt.ac.th>
2019-06-15 18:40:22 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {    
+    name : 'node32-lite',
+    platform : 'arduino-esp32',
+    title : 'NOTE32-Lite',
+    description : 'ESP32-my Doit.\n',
+    author : 'Comdet Phueadphut',
+    website : 'https://docs.espressif.com/projects/esp-idf/en/latest/get-started/get-started-wrover-kit.html',
+    email : 'sonthaya.b@honey.co.th',
+    git : 'https://github.com/jackrobotics/node32-lite/',
+    image : '/static/display.png',
+    version : '1.0.0'
+};

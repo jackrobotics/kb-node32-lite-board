bbd060aabfd94d145a6039bafda379e121b9c497
Sonthaya Boonchan <sonthaya.jackrobotic@mail.kmutt.ac.th>
2019-06-15 18:47:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
     author : 'Comdet Phueadphut',
     website : 'https://docs.espressif.com/projects/esp-idf/en/latest/get-started/get-started-wrover-kit.html',
     email : 'sonthaya.b@honey.co.th',
-    git : 'https://github.com/jackrobotics/kbpro-node32-lite-board',
+    git : 'https://github.com/jackrobotics/kb-node32-lite-board',
     image : '/static/display.png',
     version : '1.0.0'
 };

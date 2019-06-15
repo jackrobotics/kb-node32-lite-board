b33d0e44e93e991d7d7e99c5564c242fc7aabcd2
Sonthaya Boonchan <sonthaya.jackrobotic@mail.kmutt.ac.th>
2019-06-15 19:09:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     platform : 'arduino-esp32',
     title : 'NOTE32-Lite',
     description : 'Node32-lite\n',
-    author : 'Comdet Phueadphut',
+    author : 'Sonthaya Boonchan',
     website : 'https://docs.espressif.com/projects/esp-idf/en/latest/get-started/get-started-wrover-kit.html',
     email : 'sonthaya.b@honey.co.th',
     git : 'https://github.com/jackrobotics/kb-node32-lite-board/',

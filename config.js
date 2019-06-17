3e1f0743786ca407cea5a4918fe2d09ff282c532
Sonthaya Boonchan <sonthaya.jackrobotic@mail.kmutt.ac.th>
2019-06-17 16:23:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ module.exports = {
     name : 'kb-node32-lite-board',
     platform : 'arduino-esp32',
     title : 'Node32-Lite',
-    description : 'Node32-lite\n',
+    description : 'Lamloei Node32 Lite เป็นบอร์ดทดลองไมโครคอนโทรลเลอร์ที่ใช้แกนประมวลผลแบบ dual core ที่รองรับการเชื่อมต่อ WIFI และมี BT4.0 (Bluetooth Low-energy, ,BT4.0, ibeacon) เข้าไปด้วย มี GPIO ไปถึง 30 I/O ที่ทำงานได้ หลาย function ไม่ว่าจะเป็น ADC, DAC, I2C ,SPI\n',
     author : 'Sonthaya Boonchan',
     website : 'http://www.ayarafun.com/tag/node32lite/',
     email : 'sonthaya.b@honey.co.th',
     git : 'https://github.com/jackrobotics/kb-node32-lite-board/',
     image : '/static/display.png',
-    version : '1.0.3'
+    version : '1.0.4'
 };
